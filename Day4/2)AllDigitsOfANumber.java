9d6ac386747d4d79c05a6b7a2efaed3cec2a93ce
Tamanna Verma <tamannav01@gmail.com>
2021-09-06 19:48:15 +0530

    Update 2)AllDigitsOfANumber.java

--- Day4/2)AllDigitsOfANumber.java
+++ Day4/2)AllDigitsOfANumber.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+public class Main
+{
+  public static void main(String[] args)
+  {
+
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int temp = n;
+    int div = 1;
+    while (temp >10)
+    {
+      div = div * 10;
+      temp = temp / 10;
+    }
+    while (div >= 1)
+    {
+      int quotient = n / div;
+      n = n % div;
+      div = div / 10;
+      System.out.println(quotient);
+    }
+  }
+}
