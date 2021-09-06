e84defd37cfc48e4ff7f256c5fecefaa88ee9d3b
Tamanna Verma <tamannav01@gmail.com>
2021-09-06 18:47:46 +0530

    Update 1)CountDigitsInANumber.java

--- Day4/1)CountDigitsInANumber.java
+++ Day4/1)CountDigitsInANumber.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+ public class Main
+ {
+  public static void main(String[] args)
+  {
+   Scanner scan=new Scanner(System.in);
+   int n=scan.nextInt();
+   int count=0;
+   while(n!=0)
+   {
+       n=n/10;
+       count=count+1;
+   }
+   System.out.println(count);
+  }
+}
