1be9b5cb513092530ba6024c79baacf69a099046
Tamanna Verma <tamannav01@gmail.com>
2021-09-06 18:33:34 +0530

    Create 1)OddEven.java

---
+++ Day2/1)OddEven.java
@@ -0,0 +1,16 @@
+
+public class OddEven1 {
+
+	public static void main(String[] args) {
+		int x=5;
+		if(x%2==0)
+		{
+			System.out.println("It is even");
+		}
+		else 
+		{
+			System.out.println("It is odd");
+		}
+	}
+
+}
