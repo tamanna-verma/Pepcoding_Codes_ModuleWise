210237e5e67c4d7338d036cd3eaccec4e6d03175
Tamanna Verma <tamannav01@gmail.com>
2021-09-16 11:32:33 +0530

    Create CW2) Pattern 4.java

---
+++ Day 7/CW2) Pattern 4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("	");
+            }
+            for(int j=n-i;j>0;j--)
+            {
+                System.out.print("*	");
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
