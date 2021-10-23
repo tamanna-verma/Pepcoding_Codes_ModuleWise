dccfd703366ed86c5240e7e533559972b53288b9
Tamanna Verma <tamannav01@gmail.com>
2021-10-23 19:35:59 +0530

    Create 5.LastIndex.java

---
+++ Recursion In Arrays/5.LastIndex.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception     {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+           arr[i]=scan.nextInt(); 
+        }
+        int x=scan.nextInt();
+        int ans=lastIndex(arr,arr.length-1,x);
+        System.out.println(ans);
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x)
+    {
+       if(idx==0)
+       {
+           return arr[0];
+       }
+       if(arr[idx]==x)
+       {
+           return idx;
+           
+       }
+       else
+       {
+           return lastIndex(arr,idx-1,x);
+       }
+    }
+
+}
