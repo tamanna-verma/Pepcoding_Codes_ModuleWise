f7a14b1d9ff55829d8f40b9deabaf851fead4611
Tamanna Verma <tamannav01@gmail.com>
2022-01-30 11:44:38 +0530

    Create 5.PrintInRange.java

---
+++ BinarySearchTree/5.PrintInRange.java
@@ -0,0 +1,126 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static void pir(Node node, int d1, int d2) 
+  {
+   if(node==null)//agar node null hai tp simply return krdo 
+   {
+       return;
+   }
+   if(node.data<d1 && node.data<d2)//agar node ka data ,data1 aur data2 se chota hai 
+   {
+     pir(node.right,d1,d2);  //to right subtree me milega data kyuki vha node se bde elements honge
+   }
+   else if(node.data>d1 && node.data>d2)//agar node ka data data 1 aur data 2 se bda hai to left subtree me  milega data kyuki vha node se chote elements honge 
+   {
+       pir(node.left,d1,d2);
+       
+   }
+   else //otherwise hume inorder traversal krni hogi i.e phle left phit node phir right
+   {
+       pir(node.left,d1,d2);
+       System.out.println(node.data);
+       pir(node.right,d1,d2);
+       
+   }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int d1 = Integer.parseInt(br.readLine());
+    int d2 = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    pir(root, d1, d2);
+  }
+
+}
