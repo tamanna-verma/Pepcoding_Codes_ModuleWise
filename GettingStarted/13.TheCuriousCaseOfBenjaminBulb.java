8469289664fce1556491312faaddd905659a05c3
Tamanna Verma <tamannav01@gmail.com>
2022-02-16 22:23:58 +0530

    Create 13.TheCuriousCaseOfBenjaminBulb.java

---
+++ GettingStarted/13.TheCuriousCaseOfBenjaminBulb.java
@@ -0,0 +1,17 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) 
+  {
+    Scanner scan=new Scanner(System.in);
+    int n=scan.nextInt();//humne n ki value input li 
+    for(int i=1;i*i<=n;i++)
+    {
+     System.out.println(i*i);
+
+    }
+    //hume n tk k vo digit print krane hai jo ki perfect square hai 
+    
+   }
+  }
