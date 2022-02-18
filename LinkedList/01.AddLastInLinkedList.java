5abf0b63f504efa76d5c1971e9ba0b5958699d7a
Tamanna Verma <tamannav01@gmail.com>
2022-02-18 17:32:28 +0530

    Create 01.AddLastInLinkedList.java

---
+++ LinkedList/01.AddLastInLinkedList.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+
+    void addLast(int val) 
+    {
+
+      Node node=new Node();
+      node.data=val;
+      node.next=null;
+     if(size==0)
+     {
+       head=tail=node;
+     }
+     else
+     {
+       tail.next=node;
+       node.next=null;
+       tail=node;
+
+     }
+     size=size+1;
+    }
+  }
+
+  public static void testList(LinkedList list) {
+    for (Node temp = list.head; temp != null; temp = temp.next) {
+      System.out.println(temp.data);
+    }
+    System.out.println(list.size);
+
+    if (list.size > 0) {
+      System.out.println(list.tail.data);
+    } 
+  }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LinkedList list = new LinkedList();
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("addLast")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addLast(val);
+      } 
+      str = br.readLine();
+    }
+
+    testList(list);
+  }
+}
